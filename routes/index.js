56ac75b3466c001aac21db2d6741cefed4b4b2dd
Alex Yim <diulama@gmail.com>
2015-09-28 22:52:18 -0700

    Guest view of survey questions

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
 var models  = require('../models');
 var express = require('express');
+var Sequelize = require("sequelize");
 var router  = express.Router();
 
 router.get('/', function(req, res) {
-  models.Survey.findAll({
-    include: [
-      {
-        model: models.Question,
-        include: [models.Answer]
-      }
-    ]
-  }).then(function(surveys) {
+  models.Survey.find({
+    limit: 1,
+    order: Sequelize.fn('RAND'),
+    include: [{
+      model: models.Question,
+      include: [models.Answer],
+    }]
+  }).then(function(survey) {
     res.render('index', {
       title: 'Random Great Survey ',
-      surveys: surveys
+      survey: survey
     });
   });
 });
