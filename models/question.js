fef4b4f5ccbbce804cd344e349ebdbddd78ceb19
Alex Yim <diulama@gmail.com>
2015-09-28 22:58:40 -0700

    Regenerating migrations from scratch

--- models/question.js
+++ models/question.js
@@ -1,21 +1,15 @@
-"use strict";
-
+'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var Question = sequelize.define("Question", {
+  var Question = sequelize.define('Question', {
     title: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
-        Question.belongsTo(models.Survey, {
-          onDelete: "CASCADE",
-          foreignKey: {
-            allowNull: false
-          }
-        });
+        // associations can be defined here
+        Question.belongsTo(models.Survey);
         Question.hasMany(models.Answer);
       }
     }
   });
-
   return Question;
 };
--- models/survey.js
+++ models/survey.js
@@ -1,15 +1,14 @@
-"use strict";
-
+'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var Survey = sequelize.define("Survey", {
+  var Survey = sequelize.define('Survey', {
     surveyname: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
+        // associations can be defined here
         Survey.hasMany(models.Question)
       }
     }
   });
-
   return Survey;
 };
